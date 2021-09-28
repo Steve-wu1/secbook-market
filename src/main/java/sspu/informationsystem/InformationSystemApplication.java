a1cafbe5cf02bf0e525bd3f63867f431b941a2ac
cy <2418315557@qq.com>
2021-09-28 11:46:48 +0800

    test

---
+++ src/main/java/sspu/informationsystem/InformationSystemApplication.java
@@ -0,0 +1,13 @@
+package sspu.informationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformationSystemApplication.class, args);
+    }
+
+}
