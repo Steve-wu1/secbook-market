5f1bb6b9782c5b95be5e0a5fb267131acceb6664
chris <2418315557@qq.com>
2021-11-15 15:49:49 +0800

    修改页面、css、js资料位置

--- src/main/java/sspu/informationsystem/InformationSystemApplication.java
+++ src/main/java/sspu/informationsystem/InformationSystemApplication.java
@@ -1,12 +1,17 @@
 package sspu.informationsystem;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
+@MapperScan(basePackages = {"sspu.informationsystem.mapper"})
 public class InformationSystemApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(InformationSystemApplication.class, args);
     }
 
---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    if (window.sessionStorage.getItem('name') !== undefined) {
+        window.sessionStorage.removeItem('name');
+        window.sessionStorage.removeItem('phone');
+        window.sessionStorage.removeItem('customerIsLogin');
+    }
+})
+
+function login() {
+    var phone = $("#phone").val();
+    var password = $("#password").val();
+    var formData = new FormData();
+    formData.append("password", password);
+    formData.append("phone", phone);
+    $.ajax({
+        url: "http://localhost:8080/customer_login",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        header: {
+            'Acess-Control-Allow-Origin': 'http://localhost:8080'
+        },
+        success: function (data) {
+            window.sessionStorage.setItem('phone', phone);
+            window.sessionStorage.setItem('name', data);
+            window.sessionStorage.setItem('customerIsLogin', "true");
+            $(location).attr('href', '/index');
+        },
+        error: function (xhr, status, errorMessage) {
+            if (status === "501") {
+                alert("用户不存在，请注册");
+            } else if (status === "502") {
+                alert("用户名或密码错误");
+            } else {
+                alert("未知错误");
+            }
+        }
+    })
+}
+
+function ToAdminLogin() {
+    $(location).attr('href', '/adminLogin');
+}
